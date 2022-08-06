413a4b32ea548fce0d6d1f43726424c8eeaeb78e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-06 22:08:29 +0200

    info added to main.js

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const remDays = document.getElementById('days')
 const remHours = document.getElementById('hours')
 const remMinutes = document.getElementById('minutes')
 const remSeconds = document.getElementById('seconds')
+
+const birthDay = new Date ('August 05, 2022 16:00:00 GTM+1').getTime
+
+const formatTime = (Time) => (time < 10 ? `0${time}` : time);
+
+const countdown = () => {
+    const birthDayDate = new Date(birthDay);
+    const currentDate = new Date();
+}

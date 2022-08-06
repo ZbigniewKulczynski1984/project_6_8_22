e411cfb2704142f49df4403ef3c0621ca36b488b
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-06 21:48:20 +0200

    info added to main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const remDays = document.getElementById('days')
+const remHours = document.getElementById('hours')
+const remMinutes = document.getElementById('minutes')
+const remSeconds = document.getElementById('seconds')
